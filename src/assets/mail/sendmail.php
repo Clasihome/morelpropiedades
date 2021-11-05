e79cf6d2e1544ee1d352e7ba58f90112cff8397d
Ivan Solaro <ivan.solaro3@gmail.com>
2021-11-05 15:31:33 -0300

    avances

--- src/assets/mail/sendmail.php
+++ src/assets/mail/sendmail.php
@@ -2,11 +2,7 @@
     header('Access-Control-Allow-Origin: *');
     header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
     header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
-    header('content-type: application/json; charset=utf-8, ');
-
-    $rest_json = file_get_contents("php://input");
-    $_POST = json_decode($rest_json, true);
-
+    header('content-type: application/json; charset=utf-8');
 
     use PHPMailer\PHPMailer\PHPMailer;
     use PHPMailer\PHPMailer\Exception;
@@ -14,21 +10,28 @@
     require 'vendor/autoload.php';
 
     $mail = new PHPMailer(true);
+    $empresa = "Morel Propiedades";
 
-    try{
-        var_dump($_POST);
-        $name = isset( $_POST['name'] ) ? $_POST['name'] : '';
-        $email = isset( $_POST['email'] ) ? $_POST['email'] : '';
-        $phone = isset( $_POST['phone'] ) ? $_POST['phone'] : '';
-        $message = isset( $_POST['message']) ? $_POST['message'] : '';
-        $email_agent = isset( $_POST['email_agent']) && $_POST['email_agent'] != "null" ? $_POST['email_agent']: '';
-        $subject = isset( $_POST['subject']) ? $_POST['subject'] : 'contacto';
-                
+    //EMAIL HOSTING
+    $username = "no-reply@morelpropiedades.cl";
+    $pass = "Propiedades2021";
+    $host = "mail.morelpropiedades.cl";
+    // PARA PRUEBAS $_GET['email_agent'] = 'ivan.solaro3@gmail.com';
+
+
+    if( $_SERVER['REQUEST_METHOD'] == 'GET' ) {
+        
+        $name = isset( $_GET['name'] ) ? $_GET['name'] : 'No ingresado';
+        $email = isset( $_GET['email'] ) || $_GET['email'] == '' ? $_GET['email'] : 'contacto@morelpropiedades.cl';
+        $phone = isset( $_GET['phone'] ) ? $_GET['phone'] : 'No ingresado';
+        $subject = isset( $_GET['subject'] ) ? $_GET['subject'] : 'No ingresado';
+        $message = isset( $_GET['message'] ) ? $_GET['message'] : 'No ingresado';
+        $email_to = isset( $_GET['email_agent']) ? $_GET['email_agent'] : 'ivan.solaro3@gmail.com';
+
         $mensajeCompleto = 
         "Recibiste una consulta desde morelpropiedades.cl<br>
         <br>
-        <b><u>Agente:</u> </b>".$nameAgent."<br/>
-        <b><u>Asunto:</u> </b>Recibiste una consulta desde morelpropiedades.cl<br/>
+        <b><u>Asunto:</u> </b>".$subject."<br/>
         <b><u>Cliente:</u> </b>".$name."<br/>
         <b><u>Email:</u> </b><a href='mailto:".$email."'>".$email."</a><br/>
         <b><u>Celular:</u> </b>".$phone."<br/><br/>
@@ -36,43 +39,38 @@
 
         <a href='http://morelpropiedades.cl/'>www.morelpropiedades.cl</a>
         ";
-        
-        $mail->isSMTP();                     
-        $mail->Host = 'mail.morelpropiedades.cl';  
-        $mail->SMTPAuth = true;                
-        $mail->Username = 'no-reply@morelpropiedades.cl';
-        $mail->Password = 'Propiedades2021';            
-        $mail->SMTPSecure = 'ssl';                            
-        $mail->Port = 465;
-        $mail->SetFrom( $email , 'Un cliente quiere contactar con morelpropiedades.cl' );
-        $mail->addAddress( "no-reply@morelpropiedades.cl", 'Mensaje desde la web');   
-        $mail->isHTML(true);                                 
-        $mail->Subject = $subject;
-        $body = "$mensajeCompleto";
-        $mail->MsgHTML( $body );
-        $mail->CharSet = 'UTF-8';
-        if(!$mail->Send()) {
-            echo "Mailer Error: " . $mail->ErrorInfo;
-         } else {
-            echo "success";
-         }
 
-        if($email_agent != ''){
-            $mail->SetFrom( $email , 'Un cliente quiere contactar contigo' );
-            $mail->addAddress( $email_agent, 'Mensaje desde la web');   
-            $mail->isHTML(true);                                 
+      
+
+        try {
+            $mail->isSMTP();                     
+            $mail->Host = $host; //ingresar el host de mail  
+            $mail->SMTPAuth = true;                              
+            $mail->Username = $username; //ingresar nombre de usuario                 
+            $mail->Password = $pass; //ingresar pass de usuario                          
+            $mail->SMTPSecure = 'ssl';                            
+            $mail->Port = 465;                                    
+
+            $mail->SetFrom( $email , $name );
+            $mail->addAddress($email_to, $empresa);     
             $mail->Subject = $subject;
+            $mail->isHTML(true);                                 
+
             $body = "$mensajeCompleto";
             $mail->MsgHTML( $body );
-            $mail->CharSet = 'UTF-8';
-            if(!$mail->Send()) {
-                echo "Mailer Error: " . $mail->ErrorInfo;
-             } else {
-                echo "success";
-             }  
-        }
 
-    }catch (Exception $e){
-        echo "Error sending: " . $mail->ErrorInfo;
+            $mail->CharSet = 'UTF-8';
+
+            $mail->MsgHTML( $body );
+            $sendEmail = $mail->Send();
+     
+            echo json_encode(['msj' => 'Mensaje enviado, gracias por su interés!', 'status' => 1]);
+        } catch (Exception $e) {
+            echo json_encode(['msj' => 'Hubo un error al enviar el formulario. Inténtelo nuevamente más tarde'.$e, 'status' => 0]);
+        }
+    }else{
+        echo json_encode(['msj' => 'Disculpe, hubo un error. Inténtelo nuevamente más tarde', 'status' => 0]);
     }
 ?>
+
+
